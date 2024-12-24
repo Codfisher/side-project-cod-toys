a3dc3d87f6e97502746752825b86a93289c0bf12
bgm.cod <bgm.codfish@gmail.com>
2024-12-24 13:38:13 +0800

    refactor: 調整 feature.store 結構

--- src/stores/feature.store.ts
+++ src/stores/feature.store.ts
@@ -4,8 +4,46 @@ import { pipe, sortBy } from 'remeda'
 import { computed, ref } from 'vue'
 
 export const useFeatureStore = defineStore('feature', () => {
-  const selectedOptionId = ref('')
   const optionIdList = ref<string[]>([])
+  function addOptionId(id: string) {
+    optionIdList.value.push(id)
+  }
+  function removeOptionId(id: string) {
+    const index = optionIdList.value.indexOf(id)
+    if (index !== -1) {
+      optionIdList.value.splice(index, 1)
+    }
+  }
+
+  const selectedOptionId = ref('')
+  function nextOption() {
+    const first = optionIdList.value.at(0)
+
+    const index = optionIdList.value.indexOf(selectedOptionId.value)
+    if (index < 0 && first) {
+      selectedOptionId.value = first
+      return
+    }
+
+    const target = optionIdList.value[index + 1]
+    if (target || first) {
+      selectedOptionId.value = (target ?? first)!
+    }
+  }
+  function prevOption() {
+    const last = optionIdList.value.at(-1)
+
+    const index = optionIdList.value.indexOf(selectedOptionId.value)
+    if (index < 0 && last) {
+      selectedOptionId.value = last
+      return
+    }
+
+    const target = optionIdList.value[index - 1]
+    if (target || last) {
+      selectedOptionId.value = (target ?? last)!
+    }
+  }
 
   const enterHook = createEventHook<{ optionId: string }>()
   function submitEnter() {
@@ -18,45 +56,12 @@ export const useFeatureStore = defineStore('feature', () => {
       /** 依照數字排序，保證 option 順序 */
       sortBy((id) => Number.parseInt(id.replace(/\D/g, ''), 10)),
     )),
-    addOptionId(id: string) {
-      optionIdList.value.push(id)
-    },
-    removeOptionId(id: string) {
-      const index = optionIdList.value.indexOf(id)
-      if (index !== -1) {
-        optionIdList.value.splice(index, 1)
-      }
-    },
+    addOptionId,
+    removeOptionId,
 
     selectedOptionId,
-    nextOption() {
-      const first = optionIdList.value.at(0)
-
-      const index = optionIdList.value.indexOf(selectedOptionId.value)
-      if (index < 0 && first) {
-        selectedOptionId.value = first
-        return
-      }
-
-      const target = optionIdList.value[index + 1]
-      if (target || first) {
-        selectedOptionId.value = (target ?? first)!
-      }
-    },
-    prevOption() {
-      const last = optionIdList.value.at(-1)
-
-      const index = optionIdList.value.indexOf(selectedOptionId.value)
-      if (index < 0 && last) {
-        selectedOptionId.value = last
-        return
-      }
-
-      const target = optionIdList.value[index - 1]
-      if (target || last) {
-        selectedOptionId.value = (target ?? last)!
-      }
-    },
+    nextOption,
+    prevOption,
 
     submitEnter,
     onEnter: enterHook.on,
