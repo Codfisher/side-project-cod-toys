46e066b5fd6a8b5691effe8fd5390f289c123c0b
Codfisher <pony2140@livemail.tw>
2024-12-31 01:03:11 +0800

    feat: 新增 notify

--- src/pages/kaomoji-config/index.vue
+++ src/pages/kaomoji-config/index.vue
@@ -29,11 +29,13 @@
 <script setup lang="ts">
 import type { UserConfig } from '../../../electron/electron-env'
 import { useAsyncState } from '@vueuse/core'
+import { useQuasar } from 'quasar'
 import { clone } from 'remeda'
 import { ref } from 'vue'
 import { useConfigApi } from '../../composables/use-config-api'
 
 const configApi = useConfigApi()
+const $q = useQuasar()
 
 document.title = '顏文字設定'
 
@@ -60,7 +62,17 @@ function handleSubmit() {
   // clone 處理，避免 Error: An object could not be cloned
   configApi.update(clone({
     kaomoji: form.value,
-  }))
+  })).then(() => {
+    $q.notify({
+      type: 'positive',
+      message: '儲存成功',
+    })
+  }).catch(() => {
+    $q.notify({
+      type: 'negative',
+      message: '儲存失敗',
+    })
+  })
 }
 </script>
 
